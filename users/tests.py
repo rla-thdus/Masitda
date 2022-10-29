99337324296fde1e1a8d528e018f02dff8849c19
rla_thdus <0228ksy@naver.com>
2022-10-29 20:52:47 +0900

    [#11] Test: user_data setUp으로 이동

--- users/tests.py
+++ users/tests.py
@@ -3,25 +3,19 @@ from rest_framework.test import APITestCase
 
 
 class UserRegistrationAPIViewTest(APITestCase):
-    def test_registration(self):
-        user_data = {
+    def setUp(self):
+        self.user_data = {
             "email": "test@test.com",
             "password": "test1234",
             "nickname": "test",
             "address": "test시 test동 test로"
         }
 
-        response = self.client.post('/register', user_data)
+    def test_registration(self):
+        response = self.client.post('/register', self.user_data)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
     def test_same_user_register_should_return_400(self):
-        user_data = {
-            "email": "test@test.com",
-            "password": "test1234",
-            "nickname": "test",
-            "address": "test시 test동 test로"
-        }
-
-        self.client.post('/register', user_data)
-        response = self.client.post('/register', user_data)
+        self.client.post('/register', self.user_data)
+        response = self.client.post('/register', self.user_data)
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
