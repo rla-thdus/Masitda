33eb1672da6e251d79f0c22d44ac223df9f0aba2
rla_thdus <0228ksy@naver.com>
2022-10-29 20:29:44 +0900

    [#11] Feat: 회원가입 api 및 기능 추가

--- users/serializers.py
+++ users/serializers.py
@@ -1,15 +1,10 @@
 from .models import User
 from rest_framework import serializers
 
+
 class UserSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
-        user = User.objects.create_user(
-            email = validated_data['email'],
-            nickname = validated_data['nickname'],
-            address = validated_data['address'],
-            password = validated_data['password']
-        )
-        return user
+        return User.objects.create_user(**validated_data)
     class Meta:
         model = User
         fields = ['nickname', 'email', 'address', 'password']
--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,15 @@
-from django.test import TestCase
+from rest_framework import status
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+
+class UserRegistrationAPIViewTest(APITestCase):
+    def test_registration(self):
+        user_data = {
+            "email": "test@test.com",
+            "password": "test1234",
+            "nickname": "test",
+            "address": "test시 test동 test로"
+        }
+
+        response = self.client.post('/register', user_data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
