937cfb0ce08e2b668239dd15c6de31d6c0a1eabc
rla_thdus <0228ksy@naver.com>
2022-10-29 20:56:19 +0900

    [#11] Test: 유효하지 않은 데이터 형식이 들어왔을 때 동작 확인 테스트 추가

--- users/tests.py
+++ users/tests.py
@@ -10,6 +10,12 @@ class UserRegistrationAPIViewTest(APITestCase):
             "nickname": "test",
             "address": "test시 test동 test로"
         }
+        self.invalid_user_data = {
+            "email": "test@test.com@test.com",
+            "password": "test1234",
+            "nickname": "test",
+            "address": "test시 test동 test로"
+        }
 
     def test_registration(self):
         response = self.client.post('/register', self.user_data)
@@ -18,4 +24,8 @@ class UserRegistrationAPIViewTest(APITestCase):
     def test_same_user_register_should_return_400(self):
         self.client.post('/register', self.user_data)
         response = self.client.post('/register', self.user_data)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_invalid_data_should_return_400(self):
+        response = self.client.post('/register', self.invalid_user_data)
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
