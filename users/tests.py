19049a76919d209bfa3cfae49dd5c69b60bb499f
rla_thdus <0228ksy@naver.com>
2022-10-29 20:49:16 +0900

    [#11] Test: 같은 데이터로 회원가입할 때, 400 반환 검사 테스트 추가

--- users/tests.py
+++ users/tests.py
@@ -13,3 +13,15 @@ class UserRegistrationAPIViewTest(APITestCase):
 
         response = self.client.post('/register', user_data)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_same_user_register_should_return_400(self):
+        user_data = {
+            "email": "test@test.com",
+            "password": "test1234",
+            "nickname": "test",
+            "address": "test시 test동 test로"
+        }
+
+        self.client.post('/register', user_data)
+        response = self.client.post('/register', user_data)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
