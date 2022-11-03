f133f7168010a528c87e8c948f0353961d049f14
rla_thdus <0228ksy@naver.com>
2022-11-03 20:22:52 +0900

    [#31] Test: login api 테스트 클래스 분리

--- users/tests.py
+++ users/tests.py
@@ -4,7 +4,7 @@ from rest_framework import status
 from rest_framework.test import APITestCase
 
 
-class UserAccountAPIViewTest(APITestCase):
+class UserRegisterAPITest(APITestCase):
     def setUp(self):
         self.user_data = {
             "email": "test@test.com",
@@ -24,10 +24,6 @@ class UserAccountAPIViewTest(APITestCase):
             "email": "test@test.com",
             "password": "test1234",
         }
-        self.fake_info = {
-            "email": "test3@test.com",
-            "password": "test1234",
-        }
 
     def test_registration(self):
         response = self.client.post('/register', self.user_data)
@@ -42,19 +38,39 @@ class UserAccountAPIViewTest(APITestCase):
         response = self.client.post('/register', self.invalid_user_data)
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
-    def test_login(self):
-        self.client.post('/register', self.user_data)
-        response = self.client.post('/login', self.login_info)
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertIn("Token", str(response.content))
-
-    def test_with_not_registered_account_should_return_401(self):
-        response = self.client.post('/login', self.login_info)
-        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
-
     def test_logout_with_authenticated_account_should_delete_token(self):
         self.client.post('/register', self.user_data)
         user = self.client.post('/login', self.login_info)
         headers = {'HTTP_AUTHORIZATION': "token " + json.loads(user.content)['Token']}
         response = self.client.get('/logout', None, **headers)
         self.assertEqual(response.status_code, status.HTTP_202_ACCEPTED)
+
+
+class UserLoginAPITest(APITestCase):
+    def setUp(self):
+        self.user_data = {
+            "email": "test@test.com",
+            "password": "test1234",
+            "nickname": "test",
+            "phone": "+821012341234",
+            "address": "test시 test동 test로"
+        }
+        self.client.post('/register', self.user_data)
+
+        self.login_info = {
+            "email": "test@test.com",
+            "password": "test1234",
+        }
+        self.fake_info = {
+            "email": "test333@test.com",
+            "password": "test1234",
+        }
+
+    def test_login(self):
+        response = self.client.post('/login', self.login_info)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertIn("Token", str(response.content))
+
+    def test_with_not_registered_account_should_return_401(self):
+        response = self.client.post('/login', self.fake_info)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
