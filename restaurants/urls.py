2f447e5762df276d6d8d1e1a0a3155fc2d6bbeca
rla_thdus <0228ksy@naver.com>
2022-11-17 20:21:04 +0900

    [#48] Feat: 메뉴 수정 기능

--- restaurants/urls.py
+++ restaurants/urls.py
@@ -6,5 +6,6 @@ app_name = "restaurants"
 
 urlpatterns = [
     path('', views.RestaurantAPI.as_view(), name='restaurants'),
-    path('<int:restaurant_pk>/menus', views.MenuAPI.as_view(), name='menus')
+    path('<int:restaurant_pk>/menus', views.MenuAPI.as_view(), name='menus'),
+    path('<int:restaurant_pk>/menus/<int:menu_pk>', views.MenuDetailAPI.as_view())
 ]
