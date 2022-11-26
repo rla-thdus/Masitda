a8581394a7eb960f476335a5ca80c0563d4286df
rla_thdus <0228ksy@naver.com>
2022-11-26 17:10:08 +0900

    [#63] Feat: Restaurant factory faker로 값 생성

--- restaurants/factories.py
+++ restaurants/factories.py
@@ -7,6 +7,16 @@ class RestaurantFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = Restaurant
 
+    user = ''
+    name = factory.Faker('name')
+    address = factory.Faker('address')
+    phone = factory.LazyAttribute(lambda _: fake.phone_number())
+    content = factory.Faker('sentence')
+    min_order_price = factory.Faker('pyint')
+    delivery_price = factory.Faker('pyint')
+    open_time = factory.Faker('time')
+    close_time = factory.Faker('time')
+
 
 class MenuFactory(factory.django.DjangoModelFactory):
     class Meta:
