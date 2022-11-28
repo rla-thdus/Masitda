233d5850938f46bba62316b032c24cfd0be6fcd8
rla_thdus <0228ksy@naver.com>
2022-11-28 19:56:31 +0900

    [#27] Feat: 장바구니 모델 관리자 페이지에 등록

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from orders.models import Blanket
+
+
+@admin.register(Blanket)
+class BlanketAdmin(admin.ModelAdmin):
+    list_display = ['id', 'user', 'menu_id', 'count']
+
+    def menu_id(self, obj):
+        return obj.menu.id
