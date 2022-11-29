194694cd8e7efaaa472f2a52a1d09bb6c9a62973
rla_thdus <0228ksy@naver.com>
2022-11-29 20:47:16 +0900

    [#27] Feat: blanket, blanket-item serializer 추가

--- orders/serializers.py
+++ orders/serializers.py
@@ -1,11 +1,16 @@
 from rest_framework import serializers
 
-from orders.models import Blanket
+from orders.models import Blanket, BlanketItem
 
+
+class BlanketItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = BlanketItem
+        fields = '__all__'
 
 class BlanketSerializer(serializers.ModelSerializer):
     user = serializers.PrimaryKeyRelatedField(read_only=True, many=False)
-    menu = serializers.PrimaryKeyRelatedField(read_only=True, many=False)
+    blanket_items = BlanketItemSerializer(read_only=True, many=True)
 
     class Meta:
         model = Blanket
