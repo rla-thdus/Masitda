a662c721be28bdaa67d1f22134d7ea7c86053088
rla_thdus <0228ksy@naver.com>
2022-11-29 20:54:07 +0900

    [#27] Feat: admin blanketitem 관리하게 설정

--- orders/admin.py
+++ orders/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 
-from orders.models import Blanket
+from orders.models import Blanket, BlanketItem
 
 
 @admin.register(Blanket)
 class BlanketAdmin(admin.ModelAdmin):
     list_display = ['id', 'user', 'created_at']
+
+
+@admin.register(BlanketItem)
+class BlanketItemAdmin(admin.ModelAdmin):
+    list_display = ['id', 'blanket', 'menu', 'quantity']
