2b499ab6cd6312170007a56d4a6d85efe1411375
rla_thdus <0228ksy@naver.com>
2022-11-30 20:26:05 +0900

    [#27] Test: 장바구니에 메뉴 추가되는지 확인하는 테스트

--- orders/tests.py
+++ orders/tests.py
@@ -1,3 +1,24 @@
-from django.test import TestCase
+from rest_framework import status
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+from restaurants.factories import RestaurantFactory, MenuFactory
+from users.factories import UserFactory
+
+
+class BlanketAPITest(APITestCase):
+    def setUp(self):
+        self.user = UserFactory.create()
+        self.owner = UserFactory.create(role='사장')
+        self.client.force_authenticate(user=self.user)
+        self.client.force_authenticate(user=self.owner)
+        self.restaurant = RestaurantFactory.create(user=self.owner)
+        self.menu = MenuFactory.create(restaurant=self.restaurant)
+
+    def test_menu_add_in_blanket(self):
+        data = {
+            'menu': self.menu.id,
+            'quantity': 3
+        }
+        response = self.client.post(f'/blankets/{self.user.id}', data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(response.data['quantity'], 3)
