68ab3165ec4bcf951295de5929276bcaf9657711
rla_thdus <0228ksy@naver.com>
2022-12-02 18:58:12 +0900

    [#75] Refactor: carts post, get에서 blanket 사용하던 부분 cart로 수정

--- orders/views.py
+++ orders/views.py
@@ -6,7 +6,6 @@ from rest_framework.views import APIView
 from api.permissions import IsMine
 from orders.models import Cart, CartItem
 from orders.serializers import BlanketItemSerializer, BlanketSerializer
-from users.models import User
 
 
 class BlanketAPI(APIView):
@@ -14,26 +13,26 @@ class BlanketAPI(APIView):
 
     def get(self, request, user_id):
         if Cart.objects.filter(user_id=user_id).exists():
-            blanket = Cart.objects.get(user_id=user_id)
-            serializer = BlanketSerializer(blanket)
+            cart = Cart.objects.get(user_id=user_id)
+            serializer = BlanketSerializer(cart)
             return Response(serializer.data, status=status.HTTP_200_OK)
         else:
             return Response({"message": "BLANKET_NOT_EXISTS"}, status=status.HTTP_200_OK)
 
     def post(self, request, user_id):
         if Cart.objects.filter(user_id=user_id).exists():
-            blanket = Cart.objects.get(user_id=user_id)
+            cart = Cart.objects.get(user_id=user_id)
         else:
-            blanket = Cart.objects.create(user=request.user)
+            cart = Cart.objects.create(user=request.user)
 
-        blanket_item = CartItem.objects.filter(blanket_id=blanket.id, menu=request.data['menu'])
+        blanket_item = CartItem.objects.filter(cart_id=cart.id, menu=request.data['menu'])
         if blanket_item.exists():
             blanket_item.update(quantity=request.data['quantity'])
             return Response({"message": "장바구니에 해당 메뉴가 추가되었습니다."}, status=status.HTTP_200_OK)
         else:
             serializer = BlanketItemSerializer(data=request.data)
             if serializer.is_valid():
-                serializer.save(blanket=blanket)
+                serializer.save(cart=cart)
                 return Response(serializer.data, status=status.HTTP_201_CREATED)
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
