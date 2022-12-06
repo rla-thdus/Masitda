a65c0c96d2c96800defbbc65c1e2c3337dfce047
rla_thdus <0228ksy@naver.com>
2022-12-06 21:04:25 +0900

    [#73] Refactor: 장바구니 테스트 factory 사용하게 수정

--- orders/factories.py
+++ orders/factories.py
@@ -3,17 +3,17 @@ import factory
 from orders.models import Cart, CartItem
 
 
-class BlanketFactory(factory.django.DjangoModelFactory):
+class CartFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = Cart
 
     user = ''
 
 
-class BlanketItemFactory(factory.django.DjangoModelFactory):
+class CartItemFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = CartItem
 
-    blanket = ''
+    cart = ''
     menu = ''
     quantity = factory.Faker('pyint')
--- orders/tests.py
+++ orders/tests.py
@@ -1,18 +1,25 @@
 from rest_framework import status
 from rest_framework.test import APITestCase
 
+from orders.factories import CartFactory, CartItemFactory
 from restaurants.factories import RestaurantFactory, MenuFactory
 from users.factories import UserFactory
 
 
 class CartAPITest(APITestCase):
     def setUp(self):
-        self.user = UserFactory.create()
         self.owner = UserFactory.create(role='사장')
         self.client.force_authenticate(user=self.owner)
         self.restaurant = RestaurantFactory.create(user=self.owner)
         self.menu = MenuFactory.create(restaurant=self.restaurant)
-        self.client.force_authenticate(user=self.user)
+
+        self.user = UserFactory.create()
+        self.cart = CartFactory.create(user=self.user)
+        self.cart_item = CartItemFactory.create(cart=self.cart, menu=self.menu)
+
+        self.new_user = UserFactory.create()
+        self.client.force_authenticate(user=self.new_user)
+
 
     def test_menu_add_in_cart(self):
         data = {
@@ -24,11 +31,7 @@ class CartAPITest(APITestCase):
         self.assertEqual(response.data['quantity'], 3)
 
     def test_get_cart_should_return_200_when_exists_cart(self):
-        data = {
-            'menu': self.menu.id,
-            'quantity': 3
-        }
-        self.client.post(f'/carts/', data)
+        self.client.force_authenticate(user=self.user)
         response = self.client.get(f'/carts/')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data['user'], self.user.id)
@@ -39,11 +42,8 @@ class CartAPITest(APITestCase):
         self.assertEqual(response.data['message'], 'NOT_EXISTS_CART')
 
     def test_delete_exists_cart(self):
-        data = {
-            'menu': self.menu.id,
-            'quantity': 3
-        }
-        self.client.post(f'/carts/', data)
+        self.client.force_authenticate(user=self.user)
         response = self.client.delete(f'/carts/')
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
         self.assertEqual(response.data['message'], 'DELETED')
+
