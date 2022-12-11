67b4802be72e98ede680eee569a55078e7fe564d
rla_thdus <0228ksy@naver.com>
2022-12-11 23:32:14 +0900

    [#89] Feat: order status init 명령어에 주문 취소 값 추가

--- orders/management/commands/init_order_status.py
+++ orders/management/commands/init_order_status.py
@@ -10,7 +10,8 @@ class Command(BaseCommand):
         order_status = [
             '주문 접수중',
             '주문 수락',
-            '주문 거절'
+            '주문 거절',
+            '주문 취소'
         ]
         for o in order_status:
             OrderStatus.objects.create(name=o)
