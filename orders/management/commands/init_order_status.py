fecc1b531225e2feb140d2c159a890211f308f97
rla_thdus <0228ksy@naver.com>
2022-12-11 17:59:09 +0900

    [#89] Feat: 주문 요청 상태 데이터 세팅 명령어 생성

---
+++ orders/management/commands/init_order_status.py
@@ -0,0 +1,17 @@
+from django.core.management import BaseCommand
+
+from orders.models import OrderStatus
+
+
+class Command(BaseCommand):
+    help = 'init setting order status'
+
+    def handle(self, *args, **options):
+        order_status = [
+            '주문 접수중',
+            '주문 수락',
+            '주문 거절'
+        ]
+        for o in order_status:
+            OrderStatus.objects.create(name=o)
+        self.stdout.write(self.style.SUCCESS("Initial order status created"))
