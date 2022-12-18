711b5c7eb650de0e26b74c21c5c86784dc01d74b
rla_thdus <0228ksy@naver.com>
2022-12-18 20:15:31 +0900

    [#91] Test: 주문 상세 데이터 가져오는 테스트 추가

--- cores/tests/test_orders.py
+++ cores/tests/test_orders.py
@@ -22,3 +22,11 @@ class OrderAPITest(APITestCase):
     def test_order_cart_should_be_success(self):
         response = self.client.post(f'/v1/carts/{self.cart.id}/orders')
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_get_order_detail(self):
+        order = self.client.post(f'/v1/carts/{self.cart.id}/orders')
+        response = self.client.get(f'/v1/orders/{order.data["id"]}')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertTrue('total_price' in response.data)
+        self.assertTrue('delivery_price' in response.data)
+        self.assertTrue('amount_payment' in response.data)
