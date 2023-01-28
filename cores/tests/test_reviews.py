6b3bb434b5d766cbb9d6bbb699ad7b65febf1166
rla_thdus <0228ksy@naver.com>
2023-01-28 21:13:44 +0900

    [#99] Test: 자신의 주문에 대한 리뷰다는 테스트 추가

---
+++ cores/tests/test_reviews.py
@@ -0,0 +1,38 @@
+import datetime
+
+from rest_framework import status
+from rest_framework.test import APITestCase
+
+from cores.factories import RestaurantFactory, MenuFactory, OrderStatusFactory, CartFactory, CartItemFactory, \
+    OrderFactory
+from accounts.factories import UserFactory
+from cores.models import Order
+
+
+class ReviewAPITest(APITestCase):
+    def setUp(self):
+        self.owner = UserFactory.create(role='사장')
+        OrderStatusFactory.create(id=1)
+        self.accept = OrderStatusFactory.create(id=2, name='주문 수락')
+        OrderStatusFactory.create(id=3, name='주문 거절')
+        self.client.force_authenticate(user=self.owner)
+        self.restaurant = RestaurantFactory.create(user=self.owner, min_order_price=8000)
+        self.menu = MenuFactory.create(restaurant=self.restaurant, price=4000)
+
+        self.user = UserFactory.create()
+        self.cart = CartFactory.create(user=self.user, restaurant=self.restaurant)
+        self.cart.ordered_at = datetime.datetime.now(datetime.timezone.utc)
+        self.cart.save()
+        self.cart_item = CartItemFactory.create(cart=self.cart, menu=self.menu, quantity=2)
+        self.client.force_authenticate(user=self.user)
+
+        self.new_user = UserFactory.create()
+        self.order = OrderFactory(cart=self.cart, order_status=self.accept)
+        self.data = {
+            "text": "good"
+        }
+
+    def test_review_order_success(self):
+        response = self.client.post(f'/v1/orders/{self.order.id}/reviews', data=self.data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
